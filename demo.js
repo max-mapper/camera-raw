baee43ffc0dccc8af4f3e23f37d2c486abbe8dee
Max Ogden <max@maxogden.com>
2015-12-14 16:48:36 -0800

    improve metadata parsing

--- demo.js
+++ demo.js
@@ -7,6 +7,6 @@ drop(document.body.querySelector('div'), function(files) {
   var first = files[0]
   var reader = fileReaderStream(first)
   reader.pipe(concat(function (data) {
-    console.log(raw().arw(data))
+    window.foo = raw().arw(data)
   }))
 })
